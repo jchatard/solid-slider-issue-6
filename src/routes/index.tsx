0e8d31d4773a18dc98ded13181a7cdc33b73ce13
Jérémy Chatard <jchatard@breek.fr>
2022-08-11 22:59:01 +0200

    Add solid-slider example from README.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
 
+import "solid-slider/slider.css";
+import { Slider, createSlider } from "solid-slider";
+
 export default function Home() {
   return (
     <main>
@@ -14,6 +17,18 @@ export default function Home() {
         </a>{" "}
         to learn how to build SolidStart apps.
       </p>
+
+        <MyComponent></MyComponent>
     </main>
   );
 }
+
+const MyComponent = () => {
+    return (
+        <Slider options={{ loop: true }}>
+            <div>Slide 1</div>
+            <div>Slide 2</div>
+            <div>Slide 3</div>
+        </Slider>
+    );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [solid()],
+  vite: {
+    ssr: {
+      noExternal: ['solid-slider'],
+    },
+  }
 });
